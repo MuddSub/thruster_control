d498861d005128d9b6fe8a22bd164c7b76d572f3
sethgi <sisaacson@g.hmc.edu>
2019-06-26 23:36:37 -0700

    lots of nonsense

--- include/thruster_control/ThrustMaster.hpp
+++ include/thruster_control/ThrustMaster.hpp
@@ -14,7 +14,7 @@
 
 
 class Thrusters{
-	
+
 public:
 
   Thrusters();
@@ -26,64 +26,60 @@ public:
   PCA9685* pca9685;
    //thruster config
   std::unordered_map<std::string, int> thrusterConfig_;
-  
-  
-  
+
+
+
   void move();
   std::vector<std::string> thrusterNames_;
-  
- 
+
+
   int getPWM(double);
   int getPWM(std::string);
   void loadConfig();
 
   //naming: H/F [Horizontal/Vertical]; F/B [Front/Back]; R/L [Right/Left]
-  double* HFL;
-  double* HFR;
-  double* VFL;
-  double* VFR;
-  double* HBL;
-  double* HBR;
-  double* VBL;
-  double* VBR;
+  double *HFL, *HFR, *HBL, *HBR, *VFL, *VFR, *VBL, *VBR;
 
 
   // declare the subscribers
-  ros::Subscriber surgeSub, swaySub, heaveSub, pitchSub, rollSub, yawSub;
+  ros::Subscriber surgeSub, swaySub, heaveSub, pitchSub, rollSub, yawSub, enableSub;
   ros::Publisher thrustPub;
-  
-  
-  
+
+
+
 private:
 
 
 
 
 
-  
-  
+
+
   double controlEffortSurge, controlEffortSway, controlEffortHeave,
          controlEffortRoll, controlEffortPitch, controlEffortYaw;
 
-  void surgeCallback(const std_msgs::Float64& msg){ controlEffortSurge = msg.data;}
-  void swayCallback(const std_msgs::Float64& msg){ controlEffortSway = msg.data;}
-  void heaveCallback(const std_msgs::Float64& msg){ controlEffortHeave = msg.data;}
-  void pitchCallback(const std_msgs::Float64& msg){ controlEffortPitch = msg.data;}
-  void rollCallback(const std_msgs::Float64& msg){ controlEffortRoll = msg.data;}
-  void yawCallback(const std_msgs::Float64& msg){ controlEffortYaw = msg.data;}
+	bool enable;
+  double maxThrust;
 
+  inline void surgeCallback(const std_msgs::Float64& msg){ controlEffortSurge = msg.data;}
+  inline void swayCallback(const std_msgs::Float64& msg){ controlEffortSway = msg.data;}
+  inline void heaveCallback(const std_msgs::Float64& msg){ controlEffortHeave = msg.data;}
+  inline void pitchCallback(const std_msgs::Float64& msg){ controlEffortPitch = msg.data;}
+  inline void rollCallback(const std_msgs::Float64& msg){ controlEffortRoll = msg.data;}
+  inline void yawCallback(const std_msgs::Float64& msg){ controlEffortYaw = msg.data;}
+	inline void enableCallback(const std_msgs::Bool& msg){enable = msg.data;}
 
 
   std::vector<double> thrusterVals_;
 
 
 
-  
 
-  
 
-  
-  
+
+
+
+
 };
 
 
